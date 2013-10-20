c37ac69546c480c9953192a90c1e9dc58889aa97
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-10-19 23:24:21 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tictock2::Application.config.secret_key_base = '8c92658bc25b2ac6b728a824c7767d334199e78bec3e57a44ca2754b2f2774319578471c81e5ba7268c6a09dce451b5eef07b1eb16c4d03329b4f1faa11ef266'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tictock2::Application.config.session_store :cookie_store, key: '_tictock2_session'
