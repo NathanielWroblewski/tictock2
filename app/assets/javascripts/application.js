b57671e9d0d7d36e020b42449839efd9c4dbda36
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-10-20 22:16:03 -0700

    Add raphael

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 //= require_tree ./backbone/config
 //= require backbone/app
 //= require_tree ./backbone/entities
+//= require lib/raphael
 //= require_tree ./backbone/apps
 //= require foundation
 $(document).foundation();
